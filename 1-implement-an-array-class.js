8afda5460f337451c2ae418a588cb2b06a3dbcf5
Jae Kim <jckim81@yahoo.com>
2020-06-24 10:51:58 -0400

    modified 1-implement-an-array-class.js

--- 1-implement-an-array-class.js
+++ 1-implement-an-array-class.js
@@ -1,3 +1,9 @@
 //#1 Implement an Array class from scratch
+/*
+Task: Walk through each step of implementing an array. Don't rush through this by copying and pasting the code samples. 
+After you've walked through it and you understand the code of the Array class, hide the sample code and try writing 
+the Array class from scratch using the memory module here (https://gist.github.com/tparveen/c78e560c996a745dae41c1f8f2650491) for allocating memory.
 
+Be sure to export the memory module and then import it using require.
+*/
 
